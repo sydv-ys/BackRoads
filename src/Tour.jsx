4f3aa612b4b3376ec2a6b4ff53732eec3357aaae
sydv-ys <aysu.seyidova@kibrit.tech>
2024-12-25 14:10:32 +0400

    refactoring Tour component

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { tours } from "./data";
 
 function Tour() {
   return (
@@ -11,107 +12,32 @@ function Tour() {
         </div>
 
         <div class="section-center featured-center">
-          <article class="tour-card">
-            <div class="tour-img-container">
-              <img src="./images/tour-1.jpeg" class="tour-img" alt="" />
-              <p class="tour-date">august 26th, 2020</p>
-            </div>
-            <div class="tour-info">
-              <div class="tour-title">
-                <h4>Tibet Adventure</h4>
-              </div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-                vitae tempore voluptatum maxime reprehenderit eum quod
-                exercitationem fugit, qui corporis.
-              </p>
-              <div class="tour-footer">
-                <p>
-                  <span>
-                    <i class="fas fa-map"></i>
-                  </span>{" "}
-                  china
-                </p>
-                <p>6 days</p>
-                <p>from $2100</p>
-              </div>
-            </div>
-          </article>
-
-          <article class="tour-card">
-            <div class="tour-img-container">
-              <img src="./images/tour-2.jpeg" class="tour-img" alt="" />
-              <p class="tour-date">october 1th, 2020</p>
-            </div>
-            <div class="tour-info">
-              <h4>best of java</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-                vitae tempore voluptatum maxime reprehenderit eum quod
-                exercitationem fugit, qui corporis.
-              </p>
-              <div class="tour-footer">
-                <p>
-                  <span>
-                    <i class="fas fa-map"></i>
-                  </span>{" "}
-                  indonesia
-                </p>
-                <p>11 days</p>
-                <p>from $1400</p>
-              </div>
-            </div>
-          </article>
-
-          <article class="tour-card">
-            <div class="tour-img-container">
-              <img src="./images/tour-3.jpeg" class="tour-img" alt="" />
-              <p class="tour-date">september 15th, 2020</p>
-            </div>
-            <div class="tour-info">
-              <h4>explore hong kong</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-                vitae tempore voluptatum maxime reprehenderit eum quod
-                exercitationem fugit, qui corporis.
-              </p>
-              <div class="tour-footer">
-                <p>
-                  <span>
-                    <i class="fas fa-map"></i>
-                  </span>{" "}
-                  hong kong
-                </p>
-                <p>8 days</p>
-                <p>from $5000</p>
-              </div>
-            </div>
-          </article>
-
-          <article class="tour-card">
-            <div class="tour-img-container">
-              <img src="./images/tour-4.jpeg" class="tour-img" alt="" />
-              <p class="tour-date">december 5th, 2019</p>
-            </div>
-            <div class="tour-info">
-              <h4>kenya highlights</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-                vitae tempore voluptatum maxime reprehenderit eum quod
-                exercitationem fugit, qui corporis.
-              </p>
-              <div class="tour-footer">
-                <p>
-                  <span>
-                    <i class="fas fa-map"></i>
-                  </span>{" "}
-                  kenya
-                </p>
-                <p>20 days</p>
-                <p>from $3300</p>
-              </div>
-            </div>
-          </article>
+          {tours.map(({ id, date, title, text, place, days, price, img }) => {
+            return (
+              <article class="tour-card" key={id}>
+                <div class="tour-img-container">
+                  <img src={img} class="tour-img" alt="" />
+                  <p class="tour-date">{date}</p>
+                </div>
+                <div class="tour-info">
+                  <div class="tour-title">
+                    <h4>{title}</h4>
+                  </div>
+                  <p>{text}</p>
+                  <div class="tour-footer">
+                    <p>
+                      <span>
+                        <i class="fas fa-map"></i>
+                      </span>
+                      {place}
+                    </p>
+                    <p>{days}</p>
+                    <p>{price}</p>
+                  </div>
+                </div>
+              </article>
+            );
+          })}
         </div>
       </section>
     </>
--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
+
 export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
@@ -29,5 +34,48 @@ export const services = [
     icon: "fas fa-socks fa-fw",
     title: "endless hiking",
     text: " Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    place: "china",
+    days: "6 days",
+    price: "from $2100",
+    img: tour1,
+  },
+  {
+    id: 2,
+    date: "october 1th, 2020",
+    title: "best",
+    text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    place: "indonesia",
+    days: "11 days",
+    price: "from $1400",
+    img: tour2,
+  },
+  {
+    id: 3,
+    date: "september 15th, 2020",
+    title: "explore hong kong",
+    text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    place: " hong kong",
+    days: "8 days",
+    price: "from $5000",
+    img: tour3,
+  },
+  {
+    id: 4,
+    date: "december 5th, 2019",
+    title: "kenya highlights",
+    text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    place: " kenya",
+    days: "20 days",
+    price: "from $3300",
+    img: tour4,
   },
 ];
